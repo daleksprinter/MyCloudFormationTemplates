5ac61e221829c14b60ea751aa4fedbf711c558c7
daleksprinter <g2119022@fun.ac.jp>
2020-04-13 14:02:22 +0900

    add dynamodb execution role for lambda, get table data from dynamodb in lambda handler in serverless

--- serverless/lambda.py
+++ serverless/lambda.py
@@ -1,3 +1,10 @@
+import boto3
+
+dynamo = boto3.resource('dynamodb')
+
 def lambda_handler(event, context):
-    msg = "hello world"
-    return msg
+    table_name = 'User'
+    user_table = dynamo.Table(table_name)
+    res = user_table.scan()
+    item = res["Items"]
+    return item
